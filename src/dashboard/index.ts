5436747d775cd5b261bb9ad6c5ee2fa429d6672b
noClaps <github@zerolimits.dev>
2024-04-08 19:44:32 +0100

    Initial dashboard commit

---
+++ src/dashboard/index.ts
@@ -0,0 +1,11 @@
+const server = Bun.serve({
+  fetch({ url }) {
+    const path = new URL(url).pathname;
+
+    if (path === "/style.css") return new Response(Bun.file("./style.css"));
+
+    return new Response(Bun.file("./index.html"));
+  },
+});
+
+console.log(`Server started at ${server.url}.`);

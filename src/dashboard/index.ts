03878b41c4df503d2740f4ec35dc5eb23f2b2299
noClaps <github@zerolimits.dev>
2024-04-21 00:25:26 +0100

    Add rate limiting

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -10,44 +10,63 @@ const scriptLocation = "script.ts";
 
 // build script
 const script = await (
-    await Bun.build({
-        entrypoints: [join(__dirname, scriptLocation)],
-        minify: true,
-    })
+  await Bun.build({
+    entrypoints: [join(__dirname, scriptLocation)],
+    minify: true,
+  })
 ).outputs[0].text();
 
+const requestCounts = new Map();
+
 const server = Bun.serve({
-    async fetch({ url }) {
-        const path = new URL(url).pathname;
+  async fetch(req) {
+    const path = new URL(req.url).pathname;
+    const ip = server.requestIP(req);
 
-        if (path === "/") return new Response(Bun.file(join(__dirname, indexLocation)));
+    if (!requestCounts.has(ip)) {
+      requestCounts.set(ip, 0);
+    }
 
-        if (path === "/script.js") {
-            return new Response(script, {
-                headers: { "Content-Type": "application/javascript" },
-            });
-        }
+    let currentCount = requestCounts.get(ip);
+    if (currentCount > 1) {
+      return new Response("Rate limit exceeded", {
+        status: 429,
+      });
+    }
 
-        if (path === "/page") {
-            // TODO: possible DOS without a ratelimit, add one?
-            // get the page number from the query string
-            const urlObj = new URL(url);
-            const pageNum = urlObj.searchParams.has("page") ? parseInt(urlObj.searchParams.get("page")!) : 1;
+    requestCounts.set(ip, currentCount + 1);
+    setTimeout(() => requestCounts.set(ip, 0), 1000); // Reset count after 1s
 
-            // fetch the page
-            // const page = await FetchPage(pageNum); // uncomment when ready for production
-            const page = await FetchPageTest(pageNum);
-            if (!page) return new Response("Invalid page number", { status: 400 });
+    if (path === "/")
+      return new Response(Bun.file(join(__dirname, indexLocation)));
 
-            return new Response(JSON.stringify(page), {
-                headers: { "Content-Type": "application/json" },
-            });
-        }
+    if (path === "/script.js") {
+      return new Response(script, {
+        headers: { "Content-Type": "application/javascript" },
+      });
+    }
 
-        return new Response(Bun.file(join(__dirname, path)));
-    },
+    if (path === "/page") {
+      // get the page number from the query string
+      const urlObj = new URL(req.url);
+      const pageNum = urlObj.searchParams.has("page")
+        ? parseInt(urlObj.searchParams.get("page")!)
+        : 1;
 
-    port: 8146,
+      // fetch the page
+      // const page = await FetchPage(pageNum); // uncomment when ready for production
+      const page = await FetchPageTest(pageNum);
+      if (!page) return new Response("Invalid page number", { status: 400 });
+
+      return new Response(JSON.stringify(page), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response(Bun.file(join(__dirname, path)));
+  },
+
+  port: 8146,
 });
 
 console.log(`Server started at ${server.url}`);

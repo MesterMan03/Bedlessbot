bea4b8e94f78d725400ece7a185af93ff3a3c8a5
MesterMan03 <MesterMan03+github@proton.me>
2024-04-20 08:48:33 +0200

    make index callable from everywhere + cache script

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,25 +1,32 @@
+import { join } from "path";
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
+const indexLocation = "index.html";
+const scriptLocation = "script.ts";
+
+// build script
+const script = await (
+    await Bun.build({
+        entrypoints: [join(__dirname, scriptLocation)],
+        minify: true,
+    })
+).outputs[0].text();
+
 const server = Bun.serve({
-  async fetch({ url }) {
-    const path = new URL(url).pathname;
+    async fetch({ url }) {
+        const path = new URL(url).pathname;
 
-    if (path === "/")
-      return new Response(Bun.file(`${import.meta.dir}/index.html`));
+        if (path === "/") return new Response(Bun.file(join(__dirname, indexLocation)));
 
-    if (path === "/script.js") {
-      const script = await (
-        await Bun.build({
-          entrypoints: [`${import.meta.dir}/script.ts`],
-          minify: true,
-        })
-      ).outputs[0].text();
+        if (path === "/script.js") {
+            return new Response(script, {
+                headers: { "Content-Type": "application/javascript" },
+            });
+        }
 
-      return new Response(script, {
-        headers: { "Content-Type": "application/javascript" },
-      });
-    }
-
-    return new Response(Bun.file(`${import.meta.dir}${path}`));
-  },
+        return new Response(Bun.file(join(__dirname, path)));
+    },
 });
 
 console.log(`Server started at ${server.url}`);

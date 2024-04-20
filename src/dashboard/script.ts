265c6dd9a4897ad053d0eae2cf8af5872799d461
noClaps <github@zerolimits.dev>
2024-04-20 11:58:36 +0100

    Add pagination

--- src/dashboard/script.ts
+++ src/dashboard/script.ts
@@ -2,63 +2,63 @@
 
 import { FetchPageTest } from "./api-test";
 
-type User = {
-  pos: number;
-  level: number;
-  xp: number;
-  userid: string;
-  avatar: string;
-  username: string;
-  progress: number[];
-};
+async function loadUsers(pageCursor: number) {
+  const page = await FetchPageTest(pageCursor);
+  if (!page) return;
 
-let users: User[] = [];
+  for (const user of page) {
+    const podiumHTML = `<span onclick="navigator.clipboard.writeText('${user.userid}')"><img src="${user.avatar}">${user.username}</span>`;
+    const xpInfoHTML = `<span>${user.progress[0]}/${Math.floor((user.progress[0] * 100) / user.progress[1])}<br>Total xp: ${user.xp}</span>`;
+    if (user.pos === 1) {
+      const first = document.querySelector<HTMLDivElement>("#first")!;
+      first.querySelector("h2")!.innerHTML = podiumHTML;
+      first.querySelector("#xp")!.innerHTML = xpInfoHTML;
+      continue;
+    }
 
-for (let i = 0; i < 10; i++) {
-  const page = await FetchPageTest(i);
+    if (user.pos === 2) {
+      const second = document.querySelector<HTMLDivElement>("#second")!;
+      second.querySelector("h2")!.innerHTML = podiumHTML;
+      second.querySelector("#xp")!.innerHTML = xpInfoHTML;
+      continue;
+    }
 
-  if (page) {
-    users.push(...page);
-  }
-}
+    if (user.pos === 3) {
+      const third = document.querySelector<HTMLDivElement>("#third")!;
+      third.querySelector("h2")!.innerHTML = podiumHTML;
+      third.querySelector("#xp")!.innerHTML = xpInfoHTML;
+      continue;
+    }
 
-// Sorts the users in case they aren't already sorted
-users.sort((a, b) => a.pos - b.pos);
-
-for (const user of users) {
-  const podiumHTML = `<span onclick="navigator.clipboard.writeText('${user.userid}')"><img src="${user.avatar}">${user.username}</span>`;
-  const xpInfoHTML = `<span>${user.progress[0]}/${Math.floor((user.progress[0] * 100) / user.progress[1])}<br>Total xp: ${user.xp}</span>`;
-  if (user.pos === 1) {
-    const first = document.querySelector<HTMLDivElement>("#first")!;
-    first.querySelector("h2")!.innerHTML = podiumHTML;
-    first.querySelector("#xp")!.innerHTML = xpInfoHTML;
-    continue;
-  }
-
-  if (user.pos === 2) {
-    const second = document.querySelector<HTMLDivElement>("#second")!;
-    second.querySelector("h2")!.innerHTML = podiumHTML;
-    second.querySelector("#xp")!.innerHTML = xpInfoHTML;
-    continue;
-  }
-
-  if (user.pos === 3) {
-    const third = document.querySelector<HTMLDivElement>("#third")!;
-    third.querySelector("h2")!.innerHTML = podiumHTML;
-    third.querySelector("#xp")!.innerHTML = xpInfoHTML;
-    continue;
-  }
-
-  const listings = document.querySelector("section")!;
-  listings.insertAdjacentHTML(
-    "beforeend",
-    `<div class="user">
+    const listings = document.querySelector("section")!;
+    listings.insertAdjacentHTML(
+      "beforeend",
+      `<div class="user">
       <p class="pos">${user.pos}</p>
       <p class="name" onclick="navigator.clipboard.writeText('${user.userid}')"><img src="${user.avatar}">${user.username}</p>
       <label>
+        <p>Level: ${user.level}</p>
         <progress value="${user.progress[0]}" max="${(user.progress[0] * 100) / user.progress[1]}">${Math.floor((user.progress[0] * 100) / user.progress[1])}</progress>
         ${xpInfoHTML}
       </label>
     </div>`,
-  );
+    );
+  }
 }
+
+let pageCursor = 0;
+const maxPage = 10;
+
+// Initial load
+await loadUsers(pageCursor);
+pageCursor++;
+
+addEventListener("scroll", async () => {
+  if (
+    scrollY + innerHeight == document.body.clientHeight &&
+    pageCursor < maxPage
+  ) {
+    await loadUsers(pageCursor);
+    pageCursor++;
+  }
+});

448d9206e38ed924e4b231cca9222d4d14e074f7
MesterMan03 <MesterMan03+github@proton.me>
2024-04-27 13:39:28 +0200

    dont convert secret to hex, it already is

--- src/apimanager.ts
+++ src/apimanager.ts
@@ -5,11 +5,7 @@ const endpoint = process.env.API_END!;
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url).toString());
 const secret = await Bun.file(join(__dirname, "..", "secret"))
-    .arrayBuffer()
-    .then((buffer) => {
-        // convert to hex string
-        return [...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, "0")).join("");
-    })
+    .text()
     .catch((err) => {
         console.warn(err);
         return null;

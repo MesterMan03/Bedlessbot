27a1660964f809dad5a49e24fb505f1fa4843aec
MesterMan03 <MesterMan03+github@proton.me>
2024-07-11 11:27:51 +0000

    UI revamp Make comments a popout rather than a standalone component Hide packid select Add basic View Transitions API

---
+++ types/view-transitions.d.ts
@@ -0,0 +1,15 @@
+// view-transitions.d.ts
+
+interface ViewTransitionEvent extends Event {
+    transition: ViewTransition;
+  }
+  
+  interface ViewTransition {
+    updateCallbackDone: Promise<void>;
+    ready: Promise<void>;
+    finished: Promise<void>;
+  }
+  
+  interface Document {
+    startViewTransition(updateCallback: () => Promise<void> | void): ViewTransition;
+  }
